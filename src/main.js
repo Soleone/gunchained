8c4151f6cae5ab73b49c258bcb31368aa4e1b19d
Soleone <soleone@gmail.com>
2020-09-13 19:42:57 -0400

    Vuetify and Vuefire plugins

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { firestorePlugin } from "vuefire";
+import vuetify from "./plugins/vuetify";
+
+Vue.use(firestorePlugin);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
