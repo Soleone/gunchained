a0cad52f6eddf82a198823ce4c1c7c69ad93aa15
Dennis Theisen <dennis@shopify.com>
2020-09-20 23:23:56 -0400

    Change order of sign in options.

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -10,10 +10,10 @@ if (!firebase.apps.length) {
 }
 
 export const authProviders = [
-  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
   firebase.auth.EmailAuthProvider.PROVIDER_ID,
-  firebase.auth.GithubAuthProvider.PROVIDER_ID,
+  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+  firebase.auth.GithubAuthProvider.PROVIDER_ID,
   firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
   // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
   // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
