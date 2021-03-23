6018457d4129d53a82845c0350850210885acef0
Dennis Theisen <dennis@shopify.com>
2021-03-23 04:32:06 -0400

    Change order of categories

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,21 +1,21 @@
 export const VERSION = '0.2.2'
 
-export const STRATEGY = 'strategy'
+export const FUNDAMENTALS = 'fundamentals'
 export const DECKS = 'decks'
 export const CARDS = 'cards'
+export const STRATEGY = 'strategy'
 export const TRADING = 'trading'
 export const PATCH_NOTES = 'patch notes'
-export const FUNDAMENTALS = 'fundamentals'
 export const FUN = 'fun'
 export const OTHER = 'other'
 
 export const CATEGORIES = [
-  STRATEGY,
+  FUNDAMENTALS,
   DECKS,
   CARDS,
+  STRATEGY,
   TRADING,
   PATCH_NOTES,
-  FUNDAMENTALS,
   FUN,
   OTHER
 ]
