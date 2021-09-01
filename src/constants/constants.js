7194a4aebd3e1aa73d5fe7662b2d3fb2c32fc776
Soleone <soleone@gmail.com>
2021-09-01 03:34:32 -0400

    Update Youtube channel links

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -35,20 +35,23 @@ export const FEEDBACK_STATUS_COMPLETED = 'completed'
 export const FEEDBACK_STATUSES = [
   FEEDBACK_STATUS_SUBMITTED,
   FEEDBACK_STATUS_READ,
-  FEEDBACK_STATUS_COMPLETED,
+  FEEDBACK_STATUS_COMPLETED
 ]
 
 // Authors
 export const CHANNELS_BY_AUTHOR = {
-  'CopperPitch': 'https://www.youtube.com/c/copperpitch',
-  'TheProfesserf': 'https://www.youtube.com/c/theprofesserf',
-  'TeamStreamTeam': 'https://www.youtube.com/channel/UC-j8M7RvMzIbSqWIEhqT8Sg',
-  'TONYQRACER': 'https://www.youtube.com/c/TONYQRACER',
+  CopperPitch: 'https://www.youtube.com/c/copperpitch',
+  TheProfesserf: 'https://www.youtube.com/c/theprofesserf',
+  TeamStreamTeam: 'https://www.youtube.com/channel/UC-j8M7RvMzIbSqWIEhqT8Sg',
+  TONYQRACER: 'https://www.youtube.com/c/TONYQRACER',
   'Black Gaston': 'https://www.youtube.com/channel/UCcz03gAgR9mZndQnXBMpiPw',
-  'Puzzle6': 'https://www.youtube.com/channel/UC7fMQlWcusnWDeebro-3jYg',
+  Puzzle6: 'https://www.youtube.com/channel/UC7fMQlWcusnWDeebro-3jYg',
   'Pax Unchained': 'https://www.youtube.com/channel/UCocmw-0NY4xwPd3UYMEud7A',
-  'Merchant': 'https://www.youtube.com/c/MerchantTV',
-  'LionUnchained': 'https://www.youtube.com/channel/UCL_alzGOKz6hQl7UNa0mnzw',
-  'Rogvarok': 'https://www.youtube.com/channel/UCdaFKgefAytNPi-kmPOQcaw',
-  'Fakemews9': 'https://www.youtube.com/channel/UCbopgNya6aLhYi6GE8mNMvw',
+  Merchant: 'https://www.youtube.com/c/MerchantTV',
+  LionUnchained: 'https://www.youtube.com/channel/UCL_alzGOKz6hQl7UNa0mnzw',
+  Rogvarok: 'https://www.youtube.com/channel/UCdaFKgefAytNPi-kmPOQcaw',
+  Fakemews9: 'https://www.youtube.com/channel/UCbopgNya6aLhYi6GE8mNMvw',
+  TGRInteractive: 'https://www.youtube.com/channel/UCctybG40v1jj-exD5D8VqoA',
+  BadBabaGU: 'https://www.youtube.com/channel/UCgoEoyKD-kTuSQQpSQpI1aA/videos',
+  ClutchHS: 'https://www.youtube.com/channel/UCUqvAp0-3XIhx27FFt7pGIA'
 }
